1279276cb969ae1721c47aeacbbd1f93ae61a357
t0a5ted <flameburstunstoppable@gmail.com>
2023-05-09 00:05:38 +0800

    feat: everything, lol.

---
+++ index.js
@@ -0,0 +1,12 @@
+setTimeout(function(){
+    document.getElementsByTagName('body')[0].classList.add('loaded');
+}, 1000);
+
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+
