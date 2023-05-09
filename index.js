0450e4190fc0872150679e2bf23e35d487622ca9
t0a5ted <flameburstunstoppable@gmail.com>
2023-05-09 19:35:24 +0800

    feat: links, ytvid, mobile support, particlejs

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 setTimeout(function(){
     document.getElementsByTagName('body')[0].classList.add('loaded');
-}, 1000);
+}, 3000);
 
 function myFunction(imgs) {
   var expandImg = document.getElementById("expandedImg");
-  var imgText = document.getElementById("imgtext");
+  // var imgText = document.getElementById("imgtext");
   expandImg.src = imgs.src;
-  imgText.innerHTML = imgs.alt;
+  // imgText.innerHTML = imgs.alt;
   expandImg.parentElement.style.display = "block";
 }
 
+// download links with <button> instead of styling <a>
+const downloadLinks = document.querySelectorAll("[data-download]");
+downloadLinks.forEach(button => {
+  const imgpath = button.dataset.download;
+  const a = document.createElement("a");
+  a.href = imgpath;
+  a.download = ""
+  a.style.display = "none";
+
+  button.addEventListener("click", () => {
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  });
+})
